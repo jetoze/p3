90c3981207e3cd34daeb59d38da314cdc7348c98
tzeth <jetoze@gmail.com>
2018-06-30 12:07:58 -0700

    Turned Persister into a class.

--- src/main/java/p3/Persister.java
+++ src/main/java/p3/Persister.java
@@ -1,34 +1,72 @@
 package p3;
 
+import static java.util.Objects.requireNonNull;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.OptionalInt;
 import java.util.OptionalLong;
 
-public interface Persister {
-    void putString(String key, String value);
-    Optional<String> checkString(String key);
-    default String getString(String key) {
+public final class Persister {
+    private final Map<String, String> stringVals = new HashMap<>();
+    private final Map<String, Integer> intVals = new HashMap<>();
+    private final Map<String, Long> longVals = new HashMap<>();
+    
+    public void putString(String key, String value) {
+        stringVals.put(requireNonNull(key), requireNonNull(value));
+    }
+    
+    public Optional<String> checkString(String key) {
+        requireNonNull(key);
+        return Optional.ofNullable(stringVals.get(key));
+    }
+   
+    public String getString(String key) {
         return checkString(key).orElseThrow(() -> new IllegalArgumentException("No such string: " + key));
     }
-    default String getString(String key, String defVal) {
+    
+    public String getString(String key, String defVal) {
         return checkString(key).orElse(defVal);
     }
     
-    void putInt(String key, int value);
-    OptionalInt checkInt(String key);
-    default int getInt(String key) {
+    public void putInt(String key, int value) {
+        intVals.put(requireNonNull(key), value);
+    }
+    
+    public OptionalInt checkInt(String key) {
+        requireNonNull(key);
+        Integer val = intVals.get(key);
+        return (val == null)
+                ? OptionalInt.empty()
+                : OptionalInt.of(val.intValue());
+    }
+    
+    public int getInt(String key) {
         return checkInt(key).orElseThrow(() -> new IllegalArgumentException("No such int: " + key));
     }
-    default int getInt(String key, int defVal) {
+    
+    public int getInt(String key, int defVal) {
         return checkInt(key).orElse(defVal);
     }
     
-    void putLong(String key, long value);
-    OptionalLong checkLong(String key);
-    default long getLong(String key) {
+    public void putLong(String key, long value) {
+        longVals.put(requireNonNull(key), value);
+    }
+    
+    public OptionalLong checkLong(String key) {
+        requireNonNull(key);
+        Long val = longVals.get(key);
+        return (val == null)
+                ? OptionalLong.empty()
+                : OptionalLong.of(val.longValue());
+    }
+    
+    public long getLong(String key) {
         return checkLong(key).orElseThrow(() -> new IllegalArgumentException("No such long: " + key));
     }
-    default long getLong(String key, long defVal) {
+    
+    public long getLong(String key, long defVal) {
         return checkLong(key).orElse(defVal);
     }
 }
