999b892ba205c1d042f7b04fc3a29859db4056cd
tzeth <jetoze@gmail.com>
2018-06-30 11:19:18 -0700

    Fleshing out the Persister interface.

--- src/main/java/p3/Persister.java
+++ src/main/java/p3/Persister.java
@@ -1,6 +1,34 @@
 package p3;
 
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.OptionalLong;
+
 public interface Persister {
-    String getString(String key);
-    void putString(String key);
+    void putString(String key, String value);
+    Optional<String> checkString(String key);
+    default String getString(String key) {
+        return checkString(key).orElseThrow(() -> new IllegalArgumentException("No such string: " + key));
+    }
+    default String getString(String key, String defVal) {
+        return checkString(key).orElse(defVal);
+    }
+    
+    void putInt(String key, int value);
+    OptionalInt checkInt(String key);
+    default int getInt(String key) {
+        return checkInt(key).orElseThrow(() -> new IllegalArgumentException("No such int: " + key));
+    }
+    default int getInt(String key, int defVal) {
+        return checkInt(key).orElse(defVal);
+    }
+    
+    void putLong(String key, long value);
+    OptionalLong checkLong(String key);
+    default long getLong(String key) {
+        return checkLong(key).orElseThrow(() -> new IllegalArgumentException("No such long: " + key));
+    }
+    default long getLong(String key, long defVal) {
+        return checkLong(key).orElse(defVal);
+    }
 }
